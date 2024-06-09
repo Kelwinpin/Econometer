64b8673ffc03863e1fc5daa923d1978ee9c7c2d4
KelwinPin <kelwincomw@gmail.com>
2024-06-09 13:35:56 -0300

    add fetchPatterns

--- EstablishmentVersion/app/screens/Home/index.jsx
+++ EstablishmentVersion/app/screens/Home/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import { Card } from 'react-native-paper';
 import { client } from "../../utils/instance/axiosInstance";
 import { ScrollView } from "react-native-gesture-handler";
@@ -7,10 +7,16 @@ import { ScrollView } from "react-native-gesture-handler";
 export function Home() {
     const [requests, setRequests] = useState([]);
     const [requesters, setRequesters] = useState({});
+    const [patterns, setPatterns] = useState({});
 
     const getUserById = async (id) => {
         const { data } = await client.get(`/users/${id}`);
         return data.name;
+    };
+
+    const getPatternById = async (idEstablishment, idPattern) => {
+        const { data } = await client.get(`/discount-patterns/establishments/${idEstablishment}/patterns/${idPattern}`);
+        return data;
     };
 
     const fetchRequesters = async (requests) => {
@@ -24,7 +30,7 @@ export function Home() {
                     acc[userId] = result.value;
                 } else {
                     console.error(`Failed to fetch user ${userId}:`, result.reason);
-                    acc[userId] = 'Desconhecido'; 
+                    acc[userId] = 'Desconhecido'; // Fallback value
                 }
                 return acc;
             }, {});
@@ -35,10 +41,42 @@ export function Home() {
         }
     };
 
+    const fetchPatterns = async (requests) => {
+        try {
+            const patternPromises = requests.map(req => getPatternById(req.establishment_id, req.discount_pattern_id));
+            const results = await Promise.allSettled(patternPromises);
+
+            const patterns = results.reduce((acc, result, index) => {
+                const patternId = requests[index].discount_pattern_id;
+                if (result.status === "fulfilled") {
+                    acc[patternId] = result.value;
+                } else {
+                    console.error(`Failed to fetch pattern ${patternId}:`, result.reason);
+                    acc[patternId] = {
+                        rule: 'Desconhecido',
+                        discount_type: 'Desconhecido',
+                        discount_value: 0,
+                        discount_percentage: 0
+                    }; // Fallback value
+                }
+                return acc;
+            }, {});
+
+            setPatterns(patterns);
+        } catch (error) {
+            console.error("Failed to fetch patterns:", error);
+        }
+    };
+
     const cardsRequestData = async () => {
-        const res = await client.get("/discount-request/establishment/1");
-        setRequests(res.data);
-        fetchRequesters(res.data);
+        try {
+            const res = await client.get("/discount-request/establishment/1");
+            setRequests(res.data);
+            await fetchRequesters(res.data);
+            await fetchPatterns(res.data);
+        } catch (error) {
+            console.error("Failed to fetch requests data:", error);
+        }
     };
 
     useEffect(() => {
@@ -54,13 +92,24 @@ export function Home() {
                             title={`Pedido de ${req.purchase_value}`}
                             subtitle={`Pedido por ${requesters[req.user_id] || 'Carregando...'}`}
                             style={{
-                                marginBottom: 16,
+                                margin: 16,
                                 padding: 8,
                                 borderRadius: 8,
                                 width: 350,
                                 height: 59,
                             }}
                         />
+                        {patterns[req.discount_pattern_id] && (
+                            <View style={{ padding: 8 }}>
+                                <Text>Tipo de Desconto: {patterns[req.discount_pattern_id].discount_type}</Text>
+                                {patterns[req.discount_pattern_id].discount_type === "Valor Fixo" ?                                 
+                                    <Text>Valor do Desconto: ${patterns[req.discount_pattern_id].discount_value}</Text>
+                                :
+                                    <Text>Porcentagem de Desconto: {patterns[req.discount_pattern_id].discount_percentage}%</Text>
+                                }
+                                
+                            </View>
+                        )}
                     </TouchableOpacity>
                 ))}
             </ScrollView>
